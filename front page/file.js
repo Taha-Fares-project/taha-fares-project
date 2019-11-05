47993afdf29fc34a4c49dceae1dbf361cf6812c1
blusk1n <tahaznazen@gmail.com>
2019-11-05 01:27:45 +0100

    v3 added

--- front page/file.js
+++ front page/file.js
@@ -1,14 +1,11 @@
 var arr = ["2.jpg","3.jpg","1.jpg"];
-
-
-
 $(function(){
 	var i = 0;
 setInterval(function() {
       $(".allPage").css("background-image", "url(" + arr[i] + ")")
-      i = i + 1;
+      i ++
       if (i === arr.length) {
-        i =  0;
+        i = 0;
       }
 }, 3000);
 
