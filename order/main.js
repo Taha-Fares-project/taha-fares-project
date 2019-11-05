395d3aef9f918c19fe207a467a83f0cb3fee092c
blusk1n <tahaznazen@gmail.com>
2019-11-05 20:08:28 +0100

    v5

--- order/main.js
+++ order/main.js
@@ -1,3 +1,5 @@
+var array=[]
+var result = 0;
 $(function(){
 	
 	$("#btn").click(function(){
@@ -6,6 +8,14 @@ $(function(){
 //
 	$(".butt").click(function(){
 		$("section").append($(this).html())
-		
+		array.push($(this).attr("name"))
+		result = 0+result + parseInt($(this).attr("name"));
+		$("section h3").html("RESULT : "+result)
 	})
+	$("#deleteAll").click(function(){
+		$("section h1:last-child").remove()
+		$("section h3").html("RESULT : "+result)
+	})
+
+
 })
