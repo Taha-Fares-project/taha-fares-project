f6c17147036ecd81e2e8be1640230d5aca1695d5
blusk1n <tahaznazen@gmail.com>
2019-11-06 11:17:36 +0100

    last version

--- order/main.js
+++ order/main.js
@@ -1,5 +1,6 @@
 var array=[]
 var result = 0;
+var i=0;
 $(function(){
 	
 	$("#btn").click(function(){
@@ -7,15 +8,30 @@ $(function(){
 	})
 //
 	$(".butt").click(function(){
-		$("section").append($(this).html())
+
 		array.push($(this).attr("name"))
 		result = 0+result + parseInt($(this).attr("name"));
-		$("section h3").html("RESULT : "+result)
+		$("section h3").html("RESULT : "+result+" $")
+		i+=1
+
+		var deleted = "deleted"
+		$("section").append($(this).html())
+		$("section").append("<button id="+deleted+i+" style='background-color:#93291e; box-shadow:none;width:50px;height:20px;' class='deleted'>X</button>")
+		$(".deleted").click(function(){
+		$(this).prev().remove()
+		$(this).remove()
+	
+		
+	})
+
 	})
 	$("#deleteAll").click(function(){
-		$("section h1:last-child").remove()
-		$("section h3").html("RESULT : "+result)
+		$("section h1").remove()
+		$(".deleted").remove()
+		result = 0;
+		$("section h3").html("RESULT : "+result +" $")
 	})
-
-
+	
+	
 })
+
